b008902b05ad65ee61c9dc21187fb0e006ebc675
Mikhail Bahatyrou <mikhailbahatyrou@gmail.com>
2020-04-11 22:06:47 +0300

    Updated: added immutable collections, added var

---
+++ src/main/java/corporate/java/versions/immutable/ExampleImmutableCollections.java
@@ -0,0 +1,47 @@
+package corporate.java.versions.immutable;
+
+import java.util.List;
+import java.util.Map;
+
+public class ExampleImmutableCollections {
+
+    public static void main(String[] args) {
+        listOperation();
+        mapOperations();
+    }
+
+    @SuppressWarnings("all")
+    private static void listOperation() {
+        List<String> stringList = List.of(
+                "One",
+                "Two",
+                "Three"
+        );
+
+        System.out.println(stringList);
+        
+        try {
+            stringList.add("Four");
+        } catch (UnsupportedOperationException exception) {
+            System.out.println("Can't add to immutable collection");
+        } finally {
+            System.out.println(stringList);
+        }
+    }
+
+    private static void mapOperations() {
+        Map<String, Integer> integerMap = Map.of(
+                "Boolean1", 100,
+                "Boolean2", 200,
+                "Boolean3", 300);
+
+        Map<String, String> mapOfEntries = Map.ofEntries(
+                Map.entry("Str1", "001"),
+                Map.entry("Str2", "002"),
+                Map.entry("Str3", "003")
+        );
+
+        System.out.println(integerMap);
+        System.out.println(mapOfEntries);
+    }
+}
---
+++ src/main/java/corporate/java/versions/var/ExampleUseVar.java
@@ -0,0 +1,54 @@
+package corporate.java.versions.var;
+
+import java.util.ArrayList;
+
+public class ExampleUseVar {
+
+    public static void main(String[] args) {
+        var val1 = 5;
+        var val2 = 'c';
+        var val3 = "Word";
+
+        var val4 = new ArrayList<>();
+        val4.add("001");
+
+        val4.add(new MyClass(5));
+        val4.add(true);
+
+        //Bad approach to use new with var
+        var val5 = new MyClass(5);
+
+        val4.add(val5);
+
+        printValue("val1", val1);
+        printValue("val2", val2);
+        printValue("val3", val3);
+        printValue("val4", val4);
+        printValue("val5", val5);
+    }
+
+    private static void printValue(String valueName, Object value) {
+        String stringBuffer = valueName +
+                ": " +
+                "instance of: " +
+                value.getClass().getSimpleName() +
+                ", " +
+                value +
+                "\n";
+        System.out.print(stringBuffer);
+    }
+
+    static class MyClass {
+        int value;
+
+        MyClass(int value) {
+            this.value = value;
+        }
+
+        @Override
+        public String toString() {
+            return this.getClass().getSimpleName() + ", value = " + this.value;
+        }
+    }
+
+}
